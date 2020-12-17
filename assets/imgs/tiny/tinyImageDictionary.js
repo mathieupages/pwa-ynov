6c6296558af23ab7485248ee76972221925780ff
Jason <jason.liebault@simsoft-industry.fr>
2020-12-17 10:48:11 +0100

    Add lazy loading for images

---
+++ assets/imgs/tiny/tinyImageDictionary.js
@@ -0,0 +1,10 @@
+const BASE_TINY_PATH='assets/imgs/tiny/';
+
+const tinyImagesPaths = [
+    `${BASE_TINY_PATH}bordmer.jpg`,
+    `${BASE_TINY_PATH}ecluse.jpg`,
+    `${BASE_TINY_PATH}monte_perdido.jpg`,
+    `${BASE_TINY_PATH}pc.jpg`,
+    `${BASE_TINY_PATH}plaine.jpg`,
+    `${BASE_TINY_PATH}port.jpg`,
+];
--- scripts/image.js
+++ scripts/image.js
@@ -1,13 +1,25 @@
 window.onload = function () {
 	imagesPaths.forEach(imagePath => {
-		var elem = document.createElement("img");
-		elem.setAttribute("src", imagePath);
-		elem.setAttribute("alt", imagePath);
-		elem.classList.add("gallery-image");
-		document.getElementById("gallery").appendChild(elem);
-		elem.onload = () => {
-			elem.setAttribute("width", elem.width);
-			elem.setAttribute("height", elem.height);
-		}
+		const a = document.createElement("a");
+
+		a.classList.add("full");
+		a.classList.add("progressive");
+		a.classList.add("replace")
+		a.classList.add("gallery-image")
+
+		a.setAttribute("href", imagePath);
+
+		const tinyImagePath = tinyImagesPaths.filter(
+			tinyPath => {
+				const splittedImagePath = imagePath.split('/');
+				var imageName = splittedImagePath[splittedImagePath.length - 1];
+				console.log(imageName);
+				return tinyPath.includes(imageName);
+			}
+		);
+
+		a.innerHTML = `<img src="${tinyImagePath[0]}" class="preview" loading="lazy" width="20" height="15" alt="squirrel" />`
+
+		document.getElementById("gallery").appendChild(a);
 	})
 };
