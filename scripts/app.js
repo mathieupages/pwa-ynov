5c3ef69095e65eb8a588bd447af20a74d08e68e6
Jason <jason.liebault@simsoft-industry.fr>
2020-12-17 14:03:30 +0100

    Add install btn

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("../sw.js", { scope: "./" })
+        .register("../sw.js", {
+            scope: "./"
+        })
         .then(function (reg) {
             // registration worked
             console.log("Registration succeeded. Scope is " + reg.scope);
@@ -9,4 +11,34 @@ if ("serviceWorker" in navigator) {
             // registration failed
             console.log("Registration failed with " + error);
         });
+
 }
+
+window.addEventListener("onload", function (event) {
+    let deferredPrompt; // Allows to show the install prompt
+    const installButton = document.getElementById("install_button");
+    
+    window.addEventListener("beforeinstallprompt", function (event) {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+    });
+    
+    // Installation must be done by a user gesture! Here, the button click
+    installButton.addEventListener("click", (e) => {
+        // hide our user interface that shows our A2HS button
+        installButton.style.display = "none";
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt");
+            } else {
+                console.log("User dismissed the A2HS prompt");
+            }
+            deferredPrompt = null;
+        });
+    });
+});
