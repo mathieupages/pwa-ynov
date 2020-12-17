5808bcfe705134e5b1913674c9edfd142c93197a
Jason <jason.liebault@simsoft-industry.fr>
2020-12-17 14:31:39 +0100

    Fix prompt with event

--- scripts/app.js
+++ scripts/app.js
@@ -28,15 +28,15 @@ window.addEventListener("load", function (event) {
             // hide our user interface that shows our A2HS button
             installButton.style.display = "none";
             // Show the prompt
-            deferredPrompt.prompt();
+            event.prompt();
             // Wait for the user to respond to the prompt
-            deferredPrompt.userChoice.then((choiceResult) => {
+            event.userChoice.then((choiceResult) => {
                 if (choiceResult.outcome === "accepted") {
                     console.log("User accepted the A2HS prompt");
                 } else {
                     console.log("User dismissed the A2HS prompt");
                 }
-                deferredPrompt = null;
+                // deferredPrompt = null;
             });
         });
     });
