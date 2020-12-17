03bfd219d870902c7aae9cad234f2ba43e8c93ef
Jason <jason.liebault@simsoft-industry.fr>
2020-12-17 13:25:20 +0100

    rename offile html + refacto assets

--- sw.js
+++ sw.js
@@ -1,43 +1,44 @@
 const CACHE = "v1";
 
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/offline.html",
+    "/favicon.ico",
+    "/styles/",
+    "/styles/style.css",
+    "/scripts/",
+    "/scripts/app.js",
+    "/scripts/image.js",
+    "/assets/",
+    "/assets/icons/",
+    "/assets/icons/icon48.png",
+    "/assets/icons/icon72.png",
+    "/assets/icons/icon96.png",
+    "/assets/icons/icon144.png",
+    "/assets/icons/icon168.png",
+    "/assets/icons/icon192.png",
+    "/assets/imgs/",
+    "/assets/imgs/bordmer.jpg",
+    "/assets/imgs/ecluse.jpg",
+    "/assets/imgs/monte_perdido.jpg",
+    "/assets/imgs/pc.jpg",
+    "/assets/imgs/plaine.jpg",
+    "/assets/imgs/port.jpg",
+    "/assets/imgs/imageDictionnary.js",
+    "/assets/imgs/tiny/",
+    "/assets/imgs/tiny/bordmer.jpg",
+    "/assets/imgs/tiny/ecluse.jpg",
+    "/assets/imgs/tiny/monte_perdido.jpg",
+    "/assets/imgs/tiny/pc.jpg",
+    "/assets/imgs/tiny/plaine.jpg",
+    "/assets/imgs/tiny/port.jpg",
+    "/assets/imgs/tiny/tinyImageDictionnary.js",
+];
 this.addEventListener("install", function (event) {
     event.waitUntil(
         caches.open(CACHE).then(function (cache) {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/indexOffline.html",
-                "/favicon.ico",
-                "/styles/",
-                "/styles/style.css",
-                "/scripts/",
-                "/scripts/app.js",
-                "/scripts/image.js",
-                "/assets/",
-                "/assets/icons/",
-                "/assets/icons/icon48.png",
-                "/assets/icons/icon72.png",
-                "/assets/icons/icon96.png",
-                "/assets/icons/icon144.png",
-                "/assets/icons/icon168.png",
-                "/assets/icons/icon192.png",
-                "/assets/imgs/",
-                "/assets/imgs/bordmer.jpg",
-                "/assets/imgs/ecluse.jpg",
-                "/assets/imgs/monte_perdido.jpg",
-                "/assets/imgs/pc.jpg",
-                "/assets/imgs/plaine.jpg",
-                "/assets/imgs/port.jpg",
-                "/assets/imgs/imageDictionnary.js",
-                "/assets/imgs/tiny/",
-                "/assets/imgs/tiny/bordmer.jpg",
-                "/assets/imgs/tiny/ecluse.jpg",
-                "/assets/imgs/tiny/monte_perdido.jpg",
-                "/assets/imgs/tiny/pc.jpg",
-                "/assets/imgs/tiny/plaine.jpg",
-                "/assets/imgs/tiny/port.jpg",
-                "/assets/imgs/tiny/tinyImageDictionnary.js",
-            ]);
+            return cache.addAll(ASSETS);
         })
         .catch(console.error)
     );
@@ -52,16 +53,16 @@ this.addEventListener('fetch', function(event) {
             } else {
                 return fetch(event.request)     //fetch from internet
                 .then(function(res) {
-                    return caches.open(CACHE_DYNAMIC_NAME)
+                    return caches.open(CACHE)
                     .then(function(cache) {
                         cache.put(event.request.url, res.clone());    //save the response for future
                         return res;   // return the fetched data
                     })
                 })
                 .catch(function(err) {       // fallback mechanism
-                    return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                    return caches.open(CACHE)
                     .then(function(cache) {
-                        return cache.match('/indexOffline.html');
+                        return cache.match('/offline.html');
                     });
                 });
             }
