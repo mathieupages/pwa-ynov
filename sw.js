657940e57c75baf08b273ebf92317e924025bf0a
Jason <jason.liebault@simsoft-industry.fr>
2020-12-17 13:52:14 +0100

    test prompt install pwa

--- sw.js
+++ sw.js
@@ -40,26 +40,31 @@ this.addEventListener('beforeinstallprompt', function(event) {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
-    deferredPrompt = e;
+    // deferredPrompt = e;
+    e.preventDefault();
+    e.prompt();
 });
 
   // Installation must be done by a user gesture! Here, the button click
 btnAdd.addEventListener('click', (e) => {
-// hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        })
-        .catch(console.error);
+// // hide our user interface that shows our A2HS button
+//     let deferredPrompt;
+//     btnAdd.style.display = 'none';
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice
+//         .then((choiceResult) => {
+//             if (choiceResult.outcome === 'accepted') {
+//                 console.log('User accepted the A2HS prompt');
+//             } else {
+//                 console.log('User dismissed the A2HS prompt');
+//             }
+//             deferredPrompt = null;
+//         })
+//         .catch(console.error);
+    // e.preventDefault();
+    // e.prompt();
 });
 
 this.addEventListener("install", function (event) {
