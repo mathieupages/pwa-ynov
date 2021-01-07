83d4fb6f15ac35301f7bba659847f0a7714a43fd
Jason Liebault <jason.liebault@simsoft-industry.fr>
2021-01-07 09:24:52 +0100

    Add log for cache investigation

--- sw.js
+++ sw.js
@@ -55,12 +55,15 @@ this.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
+                console.log("Response from cache :", response)
                 return response;
             } else {
                 return fetch(event.request)
                     .then(function(res) {
+                        console.log("Response from web :", response)
                         return caches.open(CACHE)
                             .then(function(cache) {
+                                console.log("Response putted in the cache")
                                 cache.put(event.request.url, res.clone());
                                 return res;
                             })
