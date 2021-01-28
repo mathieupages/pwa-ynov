ad0af09bd7b63f370e1aa38ca650d9e79ef2f5c1
Jason Liebault <jason.liebault@simsoft-industry.fr>
2021-01-28 13:33:45 +0100

    Clean app logic file and explode code into new explicit formalism

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,44 +1,35 @@
-const urlB64ToUint8Array = (base64String) => {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
+function initServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('../sw.js' , {scope: './',})
+      .then(function (reg) {
+        console.log('Registration succeeded. Scope is ' + reg.scope, reg);
+        reg.onupdatefound = (event) => {
+          const installingWorker = reg.installing;
+          installingWorker.onstatechange = () => {
+            switch (installingWorker.state) {
+              case 'installed':
+                if (navigator.serviceWorker.controller) {
+                  console.log("update available");
+                  document.getElementById('new-update-message').style.display = "block";
+                }
+                break;
+            }
+          };
+        };
+      })
+      .catch(function (error) {
+        console.log('Registration failed with ' + error);
+      });
   }
-  return outputArray;
-};
+}
 
-const vapidPublicKey =
-  'BOz-y7a0En0i6slG6L-jMR6EmwTel18PAO8CLX0ECOIWeNGYo3DKXdMwN0LrmpulqE1CKl6VUMCQRW9-_7iXU8Y';
-
-const options = {
-  applicationServerKey: urlB64ToUint8Array(vapidPublicKey),
-  userVisibleOnly: true,
-};
-
-window.addEventListener('load', (e) => {
+function toggleOfflineMessage(isOffline) {
   const message = document.getElementById('offline-message');
-  if (navigator.onLine) {
-    message.style.display = 'none';
-  } else {
-    message.style.display = 'block';
-  }
-});
+  message.style.display = isOffline ? 'block' : 'none';
+}
 
-window.addEventListener('online', (event) => {
-  console.log('online');
-  document.getElementById('offline-message').style.display = 'none';
-});
-
-window.addEventListener('offline', (event) => {
-  console.log('offline');
-  document.getElementById('offline-message').style.display = 'block';
-});
-
-window.addEventListener('load', (e) => {
+function toggleInstallButton() {
   console.log(
     'match media result :',
     window.matchMedia('(display-mode: standalone)')
@@ -46,17 +37,14 @@ window.addEventListener('load', (e) => {
   if (window.matchMedia('(display-mode: standalone)').matches) {
     document.getElementById('install_button').style.display = 'none';
   }
+}
 
-  navigator.serviceWorker.ready.then(async (serviceWorkerRegistration) => {
-    const subscription = await serviceWorkerRegistration.pushManager.subscribe(
-      options
-    );
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/sub', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(subscription));
-  });
+window.addEventListener('load', (e) => {
+  initServiceWorker();
+  initGallery();
+  toggleInstallButton();
+  toggleOfflineMessage(!navigator.onLine);
+  addLoveWithPictureSubscription();
 });
 
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -76,4 +64,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
     installButton.style.display = 'none';
   });
+});
+
+window.addEventListener('online', (_event) => {
+  console.log('online');
+  toggleOfflineMessage(false);
+});
+
+window.addEventListener('offline', (_event) => {
+  console.log('offline');
+  toggleOfflineMessage(true)
 });
--- client/scripts/image.js
+++ client/scripts/image.js
@@ -1,4 +1,4 @@
-function loadImages() {
+function initGallery() {
   fetch('http://localhost:3000/paths')
     .then((resImagePaths) => {
       resImagePaths.json().then(makeGallery);
@@ -6,6 +6,56 @@ function loadImages() {
     .catch(console.error);
 }
 
-window.onload = function () {
-  loadImages();
-};
+function makeGallery(paths) {
+  paths.forEach((imagePath) => {
+    const imgHolder = makeImageHolder(imagePath);
+
+    const galleryImage = document.createElement('div');
+    galleryImage.classList.add('gallery-image');
+
+    galleryImage.append(imgHolder);
+
+    document.getElementById('gallery').appendChild(galleryImage);
+  });
+}
+
+function addLikedEvent(likeButton, imagePath) {
+  likeButton.addEventListener('click', () => {
+    fetch(`http://localhost:3000/favorite?image=${encodeURIComponent(imagePath)}`)
+      .then(subscribeLoveWithPictureNotification)
+      .catch(console.error);
+  });
+}
+
+function makeImageHolder(imagePath) {
+  const aElement = document.createElement('a');
+
+  aElement.classList.add('full');
+  aElement.classList.add('progressive');
+  aElement.classList.add('replace');
+
+  aElement.setAttribute('href', imagePath);
+
+  const tinyImagePath = tinyImagesPaths.filter((tinyPath) => {
+    const splittedImagePath = imagePath.split('/');
+    const imageName = splittedImagePath[splittedImagePath.length - 1];
+    return tinyPath.includes(imageName);
+  })[0];
+
+  aElement.innerHTML = `<img src="${tinyImagePath}" class="preview" loading="lazy" width="20" height="15" alt="preview"/>`;
+
+  const likeButton = document.createElement('button');
+  likeButton.innerHTML = '♡';
+  likeButton.classList.add('img');
+  likeButton.setAttribute('id', `${tinyImagePath}`);
+  addLikedEvent(likeButton, tinyImagePath);
+
+  const imgHolder = document.createElement('div');
+  imgHolder.classList.add('img-holder');
+
+  imgHolder.append(aElement);
+  imgHolder.append(likeButton);
+  return imgHolder;
+}
+
+
